00f6211234871fa3343c5d96a2f64ffc3b7b826b
franco@mumuki.org <franco@mumuki.org>
2021-04-09 13:47:54 +0000

    Mumuki Export on 2021-04-09 13:47:54 +0000

--- 00010_Entre los números y la pared/test.py
+++ 00010_Entre los números y la pared/test.py
@@ -1,35 +1,35 @@
 
-  def test_esta_entre_0_1_10_es_False(self):
+  def test_esta_entre_10_1_10_es_False(self):
     self.assertFalse(esta_entre(10, 1, 10))
 
   def test_esta_entre_4_4_9_es_False(self):
     self.assertFalse(esta_entre(4, 4, 9))
 
-  def test_esta_entre_2_1_10_es_False(self):
+  def test_esta_entre_12_1_10_es_False(self):
     self.assertFalse(esta_entre(12, 1, 10))
 
-  def test_esta_entre2_0_54_112_es_False(self):
+  def test_esta_entre_200_54_112_es_False(self):
     self.assertFalse(esta_entre(200, 54, 112))
 
-  def test_esta_entre_7_0_100_es_True(self):
+  def test_esta_entre_67_0_100_es_True(self):
     self.assertTrue(esta_entre(67, 0, 100))
 
   def test_esta_entre_2_0_100_es_True(self):
     self.assertTrue(esta_entre(2, 0, 100))
 
   def test_esta_fuera_de_rango_0_1_10_es_False(self):
-    self.assertFalse(esta_fuera_de_rango(10, 1, 10))
+    self.assertFalse(esta_fuera_de_rango(0, 1, 10))
 
   def test_esta_fuera_de_rango_4_4_9_es_False(self):
     self.assertFalse(esta_fuera_de_rango(4, 4, 9))
 
-  def test_esta_fuera_de_rango_2_1_10_es_True(self):
+  def test_esta_fuera_de_rango_12_1_10_es_True(self):
     self.assertTrue(esta_fuera_de_rango(12, 1, 10))
 
-  def test_esta_fuera_de_rango2_0_54_112_es_True(self):
+  def test_esta_fuera_de_rango_200_54_112_es_True(self):
     self.assertTrue(esta_fuera_de_rango(200, 54, 112))
 
-  def test_esta_fuera_de_rango_7_0_100_es_False(self):
+  def test_esta_fuera_de_rango_67_0_100_es_False(self):
     self.assertFalse(esta_fuera_de_rango(67, 0, 100))
 
   def test_esta_fuera_de_rango_2_0_100_es_False(self):
