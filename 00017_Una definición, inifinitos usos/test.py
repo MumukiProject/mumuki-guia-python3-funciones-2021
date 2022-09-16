5a5f3eef02cd6ee6d79ae2941eca123bd3034c97
rocio@mumuki.org <rocio@mumuki.org>
2022-09-16 20:57:27 +0000

    Mumuki Export on 2022-09-16 20:57:27 +0000

--- 00017_Una definición, inifinitos usos/test.py
+++ 00017_Una definición, inifinitos usos/test.py
@@ -3,5 +3,5 @@ class Test(unittest.TestCase):
   def test_Rosario_no_es_mas_largo_que_Bahia_Blanca(self):
     self.assertFalse(es_mas_largo_que("Rosario", "Bahía Blanca"))
 
-  def test_Vale_de_Uco_es_mas_largo_que_La_Punta(self):
+  def test_Valle_de_Uco_es_mas_largo_que_La_Punta(self):
     self.assertTrue(es_mas_largo_que("Valle de Uco", "La Punta"))
